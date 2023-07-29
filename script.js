3696f9ca71ceafebef378d44ed4f750785e4eaa2
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2023-07-29 02:10:48 -0300

    Add scores and a winner declared

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const avaiableChoices = ["rock", "paper", "scissors"];
   const randomNum = Math.floor(Math.random() * 3);
@@ -9,28 +12,52 @@ function playRound() {
   if (!["rock", "paper", "scissors"].includes(playerSelection)) {
     return `invalid choice`;
   }
-
   const computerSelection = getComputerChoice();
   return checkWinningCondition(playerSelection, computerSelection);
 }
 
 function checkWinningCondition(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return `Draw! Both players chose ${playerSelection}`;
+    console.log(`Draw! Both players chose ${playerSelection}`);
+    return;
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return `You win! You picked ${playerSelection} which beats ${computerSelection}`;
+    console.log(
+      `You win! You picked ${playerSelection} which beats ${computerSelection}`
+    );
+    playerScore++;
+    return;
   } else {
-    return `You lost! You picked ${playerSelection} which loses to ${computerSelection}`;
+    console.log(
+      `You lost! You picked ${playerSelection} which loses to ${computerSelection}`
+    );
+    computerScore++;
+    return;
   }
 }
 
 function game() {
+  playerScore = 0;
+  computerScore = 0;
   for (let i = 0; i < 5; i++) {
-    console.log(playRound());
+    console.log(`Round ${i + 1}:`);
+    playRound();
   }
+  console.log(
+    `--- Final score -----
+     Player: ${playerScore}, Computer: ${computerScore}`
+  );
+  console.log(
+    `${
+      playerScore > computerScore
+        ? `You won with ${playerScore} points`
+        : playerScore < computerScore
+        ? `Computer won with ${computerScore} points`
+        : `Draw`
+    }`
+  );
   //add scores
 }
